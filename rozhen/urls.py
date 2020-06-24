9bedba5b5f8166700885b22122d5fe5081f99860
Martina Bilyanska <martina.bilyanska99@gmail.com>
2020-06-24 02:45:38 +0100

    app

---
+++ rozhen/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from rozhen import views
+
+app_name = 'rozhen'
+
+urlpatterns = [
+    path('', views.index, name='index'),
+]
--- solarweb/urls.py
+++ solarweb/urls.py
@@ -15,7 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.urls import include
+from rozhen import views
 
 urlpatterns = [
+    path('', views.index, name='index'),
+    path('rozhen/', include('rozhen.urls')),
     path('admin/', admin.site.urls),
 ]
