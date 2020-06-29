d5aef6169a97a326186cdbe486bffba0a2cf1280
Martina Bilyanska <martina.bilyanska99@gmail.com>
2020-06-30 00:53:14 +0100

    some navigation changes

--- src/JavaScript/main.js
+++ src/JavaScript/main.js
@@ -1,7 +1,7 @@
 (function($){
     var header = $('#title'),
         sub = $('.subtitle'),
-        nav = $('#navbar');
+        nav = $('.navbar');
 
     TweenLite.from(header, 1,{autoAlpha: 0, x:-100, delay:0.25});
     TweenLite.from(sub, 1, {autoAlpha: 0, delay:1});
